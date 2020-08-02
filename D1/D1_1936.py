c654e5a0028ba59ca4e833aafe6e04b1f7ce59b5
rnjstpwls <kwon_sejin@naver.com>
2020-08-02 20:35:52 +0900

    20200802

---
+++ D1/D1_1936.py
@@ -0,0 +1,23 @@
+
+import sys
+
+
+sys.stdin = open("input.txt", "r")
+
+a, b = map(int, input().split())
+
+if a == 1:
+    if b == 2:
+        print('B')
+    else:
+        print('A')
+elif a == 2:
+    if b == 1:
+        print('A')
+    else:
+        print('B')
+else:
+    if b == 1:
+        print('B')
+    else:
+        print('A')
